b7b1a5ee419402e15a1727cad63b726bcdaa555f
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-07-01 00:31:04 +0100

    Finish search styling

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,8 +1,59 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Box, Button, Stack, TextField, Typography,
+} from '@mui/material';
 
 const SearchExercises = () => (
-    <div>SearchExercises</div>
+    <Stack
+      alignItems="center"
+      mt="37px"
+      justifyContent="center"
+      p="20px"
+    >
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '30px ' } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises you <br />
+        Should Know
+      </Typography>
+      <Box
+        position="relative"
+        mb="72px"
+      >
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: '#ff2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0',
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
 );
 
 export default SearchExercises;
