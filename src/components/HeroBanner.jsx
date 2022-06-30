0b3cffc351ac3662bd26e674c663b20a514ab249
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-30 23:47:07 +0100

    Styling the hero banner component

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,8 +1,66 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
+import {
+  Box, Stack, Typography, Button,
+} from '@mui/material';
+// import HeroBannerImage from '../assets/images/banner.png';
+
 const HeroBanner = () => (
-    <div>HeroBanner</div>
+    <Box
+      sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }}
+      position ="relative"
+      p="20px"
+    >
+      <Typography
+        color="#FF2625"
+        fontWeight="600"
+        fontSize="26px"
+      >
+        Fitness Club
+      </Typography>
+
+      <Typography
+        fontWeight={700}
+        mb="23px"
+        mt="30px"
+        sx={{ fontSize: { lg: '44px', xs: '40px' } }}
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+
+      <Typography
+        fontSize="22px"
+        lineHeight="35px"
+        mb={4}
+      >
+        check out the most effective exercise
+      </Typography>
+
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: '#ff2625', padding: '10px' }}
+      >
+        Explore Exercises
+      </Button>
+
+      <Typography
+        fontWeight={600}
+        fontSize="200px"
+        color="#ff2625"
+        sx={{ opacity: 0.1, display: { lg: 'block', xs: 'none' } }}
+      >
+        Exercise
+      </Typography>
+
+      <img
+        src=''
+        alt="Hero Banner image"
+        className="hero-banner-img"
+      />
+    </Box>
 );
 
 export default HeroBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+
 import { Box } from '@mui/material';
+import Exercises from '../components/Exercises.jsx';
+import SearchExercises from '../components/SearchExercises.jsx';
+import HeroBanner from '../components/HeroBanner.jsx';
 
 const Home = () => (
     <Box>
