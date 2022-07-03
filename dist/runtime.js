383e20c758f3819c24f6f706749872248ddefce4
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-07-03 11:18:06 +0100

    Compile webpack code

--- dist/runtime.js
+++ dist/runtime.js
@@ -207,11 +207,6 @@
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	(() => {
-/******/ 		__webpack_require__.nc = undefined;
-/******/ 	})();
-/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	
