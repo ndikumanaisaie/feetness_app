78b56992349b6822c0f8adaca517d9f08744dbe2
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-07-05 00:02:46 +0100

    Add exercise detail component

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExerciseVideos = () => {
+  return (
+    <div>ExerciseVideos</div>
+  )
+}
+
+export default ExerciseVideos
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,10 +6,19 @@ const exerciseOptions = {
   },
 };
 
+const youtubeOptions = {
+  method: 'GET',
+  url: 'https://youtube-search-and-download.p.rapidapi.com/video/related',
+  headers: {
+    'X-RapidAPI-Key': 'd7ec23a06fmsh3e7bf70a809778ap18b7f3jsna5e55b18f5e3',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
 const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
   return data;
 };
 
-export { exerciseOptions, fetchData };
+export { exerciseOptions, fetchData, youtubeOptions };
