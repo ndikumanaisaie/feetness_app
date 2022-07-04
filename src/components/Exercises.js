93802e9b9ad749e046abf97fabe5505b17b09e1d
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-07-04 12:19:44 +0100

    Search result and bodyPart click reflecting the change to show result element

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -19,6 +19,24 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
         window.scrollTo({top: 1800, behavior: 'smooth'})
     }
+
+    useEffect(() => {
+        const fetchExercisesData = async () => {
+            let exercisesData = [];
+
+            if (bodyPart === 'all') {
+                exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+            } else {
+                exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+
+            }
+            setExercises(exercisesData);
+        }
+
+        fetchExercisesData();
+
+    }, [bodyPart])
+
     return (
     
         <Box 
