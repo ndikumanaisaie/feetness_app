5c60028fdf1f4f4ea6e8b13ca50fef7102ee3475
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-07-10 11:18:51 +0100

    Fix API errors

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 const exerciseOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'd7ec23a06fmsh3e7bf70a809778ap18b7f3jsna5e55b18f5e3',
+		'X-RapidAPI-Key': 'f785069e79msh30a64ed9d6f5737p1a19dajsn9013dcc0b1ab',
 		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
 	},
 };
@@ -10,7 +10,7 @@ const youtubeOptions = {
 	method: 'GET',
 	url: 'https://youtube-search-and-download.p.rapidapi.com/video/related',
 	headers: {
-		'X-RapidAPI-Key': 'd7ec23a06fmsh3e7bf70a809778ap18b7f3jsna5e55b18f5e3',
+		'X-RapidAPI-Key': 'f785069e79msh30a64ed9d6f5737p1a19dajsn9013dcc0b1ab',
 		'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
 	},
 };
